daaaff71d67b51152d2f11ddd06ff49f3294f2e9
Dominik Macko <xmacko1@fi.muni.cz>
2022-07-11 15:42:02 +0200

    Add encoders documentation

--- encoders.py
+++ encoders.py
@@ -1,16 +1,47 @@
 from typing import List
 
 
-class OneHotEncoder():
+class OneHotEncoder:
+    """
+    Class that is used to one hot encode categories
+
+    Attributes
+    -----------
+    sorted_categories: List[str]
+        sorted categories that are used by encoder
+    """
 
     def __init__(self, categories: List[str]) -> "None":
+        """
+        Initializes encoder for given categories
+
+        Note: categories are sorted
+
+        Parameters
+        -----------
+        categories: List[str]
+            list of categories to encode against
+        """
         
-        self._sorted_categories = sorted(categories)
+        self.sorted_categories = sorted(list(set(categories)))
     
     def encode(self, category: str) -> List[bool]:
+        """
+        Encodes category into a list of 0s or 1s depending whether it fits supplied categories in constructor
+
+        Parameters
+        -----------
+        category: str
+            category string to encode
+        
+        Returns
+        -----------
+        List[bool]
+            boolean mask, each 1 means that category fits sorted_categories on given index
+        """
 
         return [
             category == indexed_category
             for indexed_category
-            in self._sorted_categories
+            in self.sorted_categories
             ]
